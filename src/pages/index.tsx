d392602464921e775dd4c7fd1c1536362b0504dd
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-23 12:27:45 +0530

    Updated code files: index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import FormContainer from '../components/layout/FormContainer';
 
 /**
- * LoginPage: The main page component for the login screen.
- * It sets up the overall layout, centering the login form vertically and horizontally.
+ * SignupPage: The main page component for the signup screen.
+ * It sets up the overall layout, centering the signup form vertically and horizontally.
  */
-const LoginPage: React.FC = () => {
+const SignupPage: React.FC = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-background p-4">
       <FormContainer />
@@ -13,4 +13,4 @@ const LoginPage: React.FC = () => {
   );
 };
 
-export default LoginPage;
+export default SignupPage;
